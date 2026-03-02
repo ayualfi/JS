0fe48a8571a9ad3f2deadc2321322e1b44ecfad4
Ayu Alfi <ayu425537@gmail.com>
2026-03-02 12:45:21 +0700

    membuat gunung hadao ke kanan

--- latihan1.js
+++ latihan1.js
@@ -1,8 +1,11 @@
 var string = ''
+for(var i = 0; i < 10 ; i++){
+    for(var j = 0; j <= i ; j++){
+        string = string + '#'
+    } 
+    string = string + '\n'
+}
 for(var i = 10; i > 0 ; i--){
-    for(var k = 0; k<10-i ; k++){
-        string = string + ' '
-    }
     for(var j = 0; j < i ; j++){
         string = string + '#'
     } 
