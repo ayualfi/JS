b7ebf7c10b8948f121dcd6ab61d13b45488fee4b
Ayu Alfi <ayu425537@gmail.com>
2026-03-02 12:38:24 +0700

    membuat tangga bintang dari bawah

--- latihan1.js
+++ latihan1.js
@@ -1,5 +1,8 @@
 var string = ''
 for(var i = 10; i > 0 ; i--){
+    for(var k = 0; k<10-i ; k++){
+        string = string + ' '
+    }
     for(var j = 0; j < i ; j++){
         string = string + '#'
     } 
