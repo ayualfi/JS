28c30b9dfc58a800740cb33928bf248181145a7d
Ayu Alfi <ayu425537@gmail.com>
2026-03-04 12:42:47 +0700

    ini segitiga sama kaki kebalik

--- latihan1.js
+++ latihan1.js
@@ -1,13 +1,13 @@
 var string = ''
 var tinggi = 7
 
-for ( var i = 1 ; i <= tinggi ; i++){
+for ( var i = tinggi ; i >= 1 ; i--){
 //spasi
-    for (var k = tinggi ; k > i ; k--){
+    for (var k = tinggi; k > i ; k--){
         string += " "
     }
 // pagar
-    for (var j = 1 ; j <= (2 * i - 1 ); j++){
+    for (var j = 1 ; j <= (2 * i - 1) ; j++){
     string += "#"
 }
     string += '\n'
