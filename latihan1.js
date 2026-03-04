70b757681cace41650595ad6a90759b43310a437
Ayu Alfi <ayu425537@gmail.com>
2026-03-04 13:06:00 +0700

    membuat diamond

--- latihan1.js
+++ latihan1.js
@@ -1,7 +1,16 @@
 var string = ''
 var tinggi = 7
 
-for ( var i = tinggi ; i >= 1 ; i--){
+for (var i = 1 ; i <= tinggi ; i ++){
+    for(var k = tinggi ; k > i ; k--){
+        string += " "
+    }
+    for(var j = 1 ; j <= (2 * i - 1) ; j++){
+        string += "#"
+    }
+    string += '\n'
+}
+for ( var i = tinggi-1 ; i >= 1 ; i--){
 //spasi
     for (var k = tinggi; k > i ; k--){
         string += " "
