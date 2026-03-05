614266678982f5996e3e9eb78e3094efe20e00ed
Ayu Alfi <ayu425537@gmail.com>
2026-03-05 13:10:13 +0700

    lagi males mikirtapi harus ngoding hehe

--- latihan1.js
+++ latihan1.js
@@ -1,24 +1,10 @@
 var string = ''
-var tinggi = 7
+for (var x = 1 ; x < 10 ; x++){
+    for (var i = 1 ; i < 10 ; i++){
+        string += "*"
+}
+string += "\n"
+}
+console.log(string)
 
-for (var i = 1 ; i <= tinggi ; i ++){
-    for(var k = tinggi ; k > i ; k--){
-        string += " "
-    }
-    for(var j = 1 ; j <= (2 * i - 1) ; j++){
-        string += "#"
-    }
-    string += '\n'
-}
-for ( var i = tinggi-1 ; i >= 1 ; i--){
-//spasi
-    for (var k = tinggi; k > i ; k--){
-        string += " "
-    }
-// pagar
-    for (var j = 1 ; j <= (2 * i - 1) ; j++){
-    string += "#"
-}
-    string += '\n'
-}
-console.log (string)
+
