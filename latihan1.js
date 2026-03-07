704640de75687e5770154b6a5ac33b9f63c32cf4
Ayu Alfi <ayu425537@gmail.com>
2026-03-07 20:55:19 +0700

    membuat gunung fibonaci tapi belum simetris

--- latihan1.js
+++ latihan1.js
@@ -1,10 +1,25 @@
 var string = ''
-for (var x = 1 ; x < 10 ; x++){
-    for (var i = 1 ; i < 10 ; i++){
-        string += "*"
+var tinggi = 4
+var a = 0
+var b = 1
+
+for (var x = 1 ; x <= tinggi ; x++){
+    for (var k = tinggi ; k > x ; k--){
+        string += ' '
+    }
+    for (var i = 0 ; i < x ; i++){
+        string += (b)
+        var c = a + b
+        a = b
+        b = c
+        if (i != x-1){
+            string += ' '
+        }
+        
+    }
+    string += '\n'
 }
-string += "\n"
-}
+
 console.log(string)
 
 
