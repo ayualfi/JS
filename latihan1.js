6953689ca0cdc29b54181892b5efd4afdc81d082
Ayu Alfi <ayu425537@gmail.com>
2026-03-08 10:34:21 +0700

    Membuat segitiga pascal dari dua ngka diatasnya

--- latihan1.js
+++ latihan1.js
@@ -2,12 +2,13 @@ var string = ''
 var tinggi = 4
 
 for (var x = 0 ; x <= tinggi ; x++){
+    var num = 1
+    for (var j = tinggi ; j > x ; j--){
+        string +=' '
+    }
     for (var k = 0 ; k <= x ; k++){
-        if (k == 0 || k == x ){
-            string += 1
-        }else{
-            string += 2
-        }
+        string += (num) += ' '
+        num = num * (x-k)/(k+1)  
     }
     string += '\n'
 }
