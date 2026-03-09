868d2cf0025fc223fd4ff74239511479c75a7ef0
Ayu Alfi <ayu425537@gmail.com>
2026-03-09 11:37:14 +0700

    ini fix

--- tebakangka/tebakangka.js
+++ tebakangka/tebakangka.js
@@ -1,40 +1,30 @@
 // input tebakan player
 var namaPlayer = prompt('Halo, whats your name ?')
-var player = prompt('Tebaklah angka antara 1 - 10 ' + namaPlayer+ '! \nkamu punya 3 kesempatan')
 // pilihan komputer
 var comp = Math.floor(Math.random()*10)+1
 console.log(comp)
 // aturan pilihan benar
-var hasil = hasil
-var hasil1 = hasil1
-
-if (player < 1 || player > 10){
-    hasil1 = namaPlayer + ' tidak memasukkan angka antara 1 - 10'
-}else if (player < comp){
-    hasil1 = 'Terlalu rendah'
-}else if (player > comp){
-    hasil1 = 'Terlalu tinggi'
-}else{
-    hasil1 = 'benar'
-}
-
-// nyawa player
-
-if (hasil1 =='Terlalu rendah' || hasil1 == 'Terlalu tinggi'){
-    var player = prompt (hasil1 + '! Tebaklah lagi ' + namaPlayer + '!')
-    if (player == comp){
-        hasil = 'Benar'
-    }else{
-        var player = prompt (hasil1 + '! Tebaklah lagi ' + namaPlayer + '!')
+var ulangi = true
+while (ulangi){
+    var kesempatan = 3
+    var jawaban = false
+    while(kesempatan > 0 && jawaban == false){
+        var player = prompt('Pilihlah angka antara 1 - 10 ' + namaPlayer+ '! \nkamu punya ' + kesempatan + ' kesempatan')
+        
         if (player == comp){
-            hasil = 'Benar'
-        }else{
-            hasil ='Nyawa habis! \nkamu kalah '+namaPlayer+' angka seharusnya = '+comp
+            jawaban = true
+            alert('Kamu benar ' + namaPlayer + '!') 
+        }else if(Math.abs(player-comp)==1){
+            alert('Hampir benar')
+        }else if(player > comp){
+            alert('Terlalu tinggi')
+        }else if(player < comp){
+            alert('Terlalu rendah')
         }
+    kesempatan--
     }
+    if (jawaban == false){
+        alert('Kesempatan habis '+ namaPlayer +'!, angka seharusnya = ' + comp)
+    }
+ulangi = confirm (namaPlayer + ' mau main lagi ?')
 }
-
-// hasil tebakan
-alert(hasil1)
-
-
