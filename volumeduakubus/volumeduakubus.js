0ae7cc24c37efdc7acc5496e967bdbd63b7a859d
Ayu Alfi <ayu425537@gmail.com>
2026-03-12 22:39:06 +0700

    fungsi menghitung jumlah volume 2 kubus

---
+++ volumeduakubus/volumeduakubus.js
@@ -0,0 +1,23 @@
+namaPlayer=prompt('Halo!, bagaimana aku memanggilmu? ')
+var konfirmasi = confirm('Halo ' + namaPlayer + '\nKamu mau menghitung total volume kedua kubusmu?')
+
+while (konfirmasi==true){
+function menghitungTotalVolumeDuaKubus (a, b){
+    var volumeKubusA
+    var volumeKubusB
+    var total
+
+    volumeKubusA = a * a * a
+    volumeKubusB = b * b * b
+    total = volumeKubusA + volumeKubusB
+
+    return total
+}
+
+a=prompt('Berapa sisi kubus 1 = ')
+b=prompt('Berapa sisi kubus 2 = ')
+
+alert('Total volume kedua kubus '+namaPlayer + ' = '+menghitungTotalVolumeDuaKubus(a, b))
+konfirmasi=confirm('Mau menghitung total volume kedua kubus lagi '+namaPlayer+' ?')
+}
+
