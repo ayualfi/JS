0bff10ec20a9c796413cff84b5b4d705c5a8d6cf
Ayu Alfi <ayu425537@gmail.com>
2026-03-19 18:47:08 +0700

    manipulasi element array dngan cara manual

---
+++ belajar/manipulasiArray.js
@@ -0,0 +1,25 @@
+// Array
+
+// var arr = ['Alfi', 1, 'Tian', true]
+// console.log(arr)
+
+//Menambah element pada array
+// var arr = []
+// arr[0] = 'Ayu'
+// arr[1] = 1
+// arr[2] = 'Tian'
+// arr[3] = true
+// console.log(arr)
+
+// menghapus element pada array
+// var arr = ['Alfi', 1, 'Tian', true]
+// arr[1]=undefined
+// console.log(arr)
+
+//Menampilkan element array dengan tidak menggunakan objek
+var arr = ['Alfi', 1, 'Tian', true]
+
+for (var i = 0; i < arr.length; i++){
+    console.log('Element ke '+ (i + 1) + ' : ' + arr[i])
+}
+
