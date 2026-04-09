b53d04799df907261de2a1898acbf6aae8d10f14
Ayu Alfi <ayu425537@gmail.com>
2026-04-09 16:23:57 +0700

    belajar cara menambah elemnt dengan js

--- belajarDOM/DOMManipulation.js
+++ belajarDOM/DOMManipulation.js
@@ -3,25 +3,46 @@
 // ==> akan menimpa
 // ==> bisa memasukkan tag html
 
-const p4 = document.querySelector('section#s2 p')
-p4.innerHTML = ('<strong> Ayu Alfi Hidayati </strong> <section></section>')
+// const p4 = document.querySelector('section#s2 p')
+// p4.innerHTML = ('<strong> Ayu Alfi Hidayati </strong> <section></section>')
 
-// element.style
-// ==> styling seperti css menggunakan atribut css
+// // element.style
+// // ==> styling seperti css menggunakan atribut css
 
-const judul = document.getElementById('judul')
-judul.style.backgroundColor = 'green'
-
-
-// element.setAttribut()
-// menambah atribut
-const p1 = document.getElementsByTagName('div')[0]
-p1.setAttribute('class', 'div1')
+// const judul = document.getElementById('judul')
+// judul.style.backgroundColor = 'green'
 
 
-// element.getAttribut()
-// ==> mengembalikan isi atribut
-const ig = document.querySelector('section.sel1 a')
+// // element.setAttribut()
+// // menambah atribut
+// const p1 = document.getElementsByTagName('div')[0]
+// p1.setAttribute('class', 'div1')
+
+
+// // element.getAttribut()
+// // ==> mengembalikan isi atribut
+// const ig = document.querySelector('section.sel1 a')
 
 // element.removeAttribute
 // ==> menghapus atribut
+
+// element.classLis
+// 1. element.classList.add()
+// ==> menambah class dalam sebuah element
+
+// var paragraf4 = document.querySelector('section#s2 p')
+
+
+// === part 2 ======
+// document.createElement
+
+// buat element baru
+const pBaru = document.createElement('p')
+const textPBaru = document.createTextNode('Paragraf baru')
+//simpan tulisan ke element
+pBaru.appendChild(textPBaru)
+// siman pbaru ke akhir section a
+const p = document.getElementsByClassName('sel1')[0]
+p.appendChild(pBaru)
+
+
