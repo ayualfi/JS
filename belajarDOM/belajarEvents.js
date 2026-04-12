334c61c42e45bfa9ad5d66188a922eba7699af59
Ayu Alfi <ayu425537@gmail.com>
2026-04-12 15:39:52 +0700

    addeventlistener

--- belajarDOM/belajarEvents.js
+++ belajarDOM/belajarEvents.js
@@ -4,9 +4,22 @@ const judul = document.getElementById('judul')
 
 function ubahBg (){
     judul.style.backgroundColor = 'lightblue'
+    judul.style.backgroundColor = 'lightgreen'
 }
 // 2. element method
 judul.onclick = ubahBg
 
 // addEventListener
-const p4 = document.querySelector('section#s2, p')
+const p4 = document.querySelector('section#s2 p')
+
+p4.addEventListener ('click', function (){
+    const liBaru = document.createElement('li')
+    const teksLiBaru = document.createTextNode('Li baru')
+    liBaru.appendChild(teksLiBaru)
+
+    const ul = document.querySelector('ul')
+    ul.appendChild(liBaru)
+
+    liBaru.style.backgroundColor = 'lightgreen'
+})
+
