995ac4d1461deb4d85bfd6f16b099631998d1342
Ayu Alfi <ayu425537@gmail.com>
2026-04-15 12:29:11 +0700

    ubah warna random

--- belajarDOM/mainWarna.js
+++ belajarDOM/mainWarna.js
@@ -6,3 +6,19 @@ function ubahWarnaBg (){
     klikButton.classList.toggle('button')
 }
 klikButton.onclick = ubahWarnaBg
+
+// tombol untuk mengubah warna background menjadi warna random
+// buat tombol
+const tombolBaru = document.createElement('button')
+const textTombolBaru = document.createTextNode('ganti warna')
+tombolBaru.appendChild(textTombolBaru)
+tombolBaru.setAttribute('type', 'button')
+klikButton.after(tombolBaru)
+
+
+tombolBaru.addEventListener('click', function(){
+    const r = Math.round(Math.random() * 255 + 1)
+    const g = Math.round(Math.random() * 255 + 1)
+    const b = Math.round(Math.random() * 255 + 1)
+    document.body.style.backgroundColor='rgb('+ r +','+ g +','+ b +')'
+})
