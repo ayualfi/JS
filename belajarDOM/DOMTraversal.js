79a46a5e9141f50f42347b184c8ae0a238807ab5
Ayu Alfi <ayu425537@gmail.com>
2026-04-22 13:22:21 +0700

    mencoba menggunakan dom traversal

---
+++ belajarDOM/DOMTraversal.js
@@ -0,0 +1,13 @@
+// menggunakan dom manipulation
+
+// const tombolHapus = document.querySelector('.x')
+// const kontakCard = document.querySelector('.card')
+
+// tombolHapus.addEventListener('click', function(){
+//     kontakCard.style.display = 'none';
+// })
+
+// menggunakan dom traversal
+
+const tombolHapus = document.querySelectorAll('.x')
+const kontakCard = document.querySelectorAll('.card')
